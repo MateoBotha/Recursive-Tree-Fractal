effe3ee97f31e646a182c9292f2ad8b7dfc0dce6
Mateo <mateobotha@gmail.com>
2023-08-06 15:05:58 +0200

    Trying to get the SubBranch to have a child of another SubBranch... Failing weirdly, but I think I'm close

--- src/com/oreconsultants/FractalWindow.java
+++ src/com/oreconsultants/FractalWindow.java
@@ -3,6 +3,7 @@ package com.oreconsultants;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 
 public class FractalWindow extends JFrame {
     FractalWindow() {
@@ -31,9 +32,11 @@ public class FractalWindow extends JFrame {
             draw(g);
         }
         public void draw(Graphics g) {
-            SubBranch subBranch = new SubBranch(n, 40, (Branch) null,null);
-            Branch branch = new Branch(new SubBranch[]{subBranch},20,new Point(Screen_Width/2,Screen_Height/2),0);
+            SubBranch subBranch = new SubBranch(-45, 40, (Branch) null,null);
+            SubBranch subBranch2 = new SubBranch(n, 20, (Branch) null,null);
+            Branch branch = new Branch(new SubBranch[]{subBranch,subBranch2},20,new Point(Screen_Width/2,Screen_Height/2),0);
             subBranch.setBranchParent(branch);
+            subBranch2.setSubParent(subBranch);
             drawBranch(g,branch);
             n++;repaint();
         }
@@ -43,24 +46,38 @@ public class FractalWindow extends JFrame {
             double branchLength = branch.getLength();
             double branchAngle = branch.getAngle();
             SubBranch[] subBranches = branch.getSubBranches();
-            {
+
             g2d.translate(branchCoordinates.x, branchCoordinates.y);
             AffineTransform old = g2d.getTransform();
             g2d.rotate(Math.toRadians(branchAngle));
             g2d.fillRect((int) -(branchThickness/2), (int) -(branchLength/2), (int)branchThickness, (int) branchLength);
-            g2d.setTransform(old);
+            //g2d.setTransform(old);
             g2d.translate(-branchCoordinates.x, -branchCoordinates.y);
-            }
+
             for (int i = 0; i < subBranches.length; i++) {
-                branchThickness-=branchThicknessDecreaseAmount;
-                Point subBranchCoordinates = new Point((branchCoordinates.x), (int) (branchCoordinates.y-(branchLength)/2));
-                AffineTransform old = g2d.getTransform();
-                g2d.translate(subBranchCoordinates.x,subBranchCoordinates.y);
-                g2d.rotate(Math.toRadians(subBranches[i].getAngle()));
-                g2d.fillRect((int) -(branchThickness/2),(int)-(subBranches[i].getLength()),(int)branchThickness,(int)subBranches[i].getLength());
-                g2d.setTransform(old);
-                //g2d.translate(-(subBranchCoordinates.x+((double)branchThickness/2)),-(subBranchCoordinates.y+(branchLength/2)));
+                if (!subBranches[i].getParentType()) {
+                    // the current Subbranch has a branch as a parent [the branch we just drew]
+
+                    Point subBranchCoordinates = new Point((branchCoordinates.x), (int) (branchCoordinates.y - (branchLength) / 2));
+                    //AffineTransform old = g2d.getTransform();
+                    g2d.translate(subBranchCoordinates.x, subBranchCoordinates.y);
+                    g2d.rotate(Math.toRadians(subBranches[i].getAngle()));
+                    g2d.fillRect((int) -(branchThickness / 2), (int) -(subBranches[i].getLength()), (int) branchThickness, (int) subBranches[i].getLength());
+                    //g2d.setTransform(old);
+                    //g2d.translate(-(subBranchCoordinates.x+((double)branchThickness/2)),-(subBranchCoordinates.y+(branchLength/2)));
+                } else {
+                    // the current subBranch has a subbranch as a parent, we must draw it on top of its parent.
+                    SubBranch currentSubBranchParent = subBranches[i].getSubParent();
+                    System.out.println(g2d.getTransform().transform(new Point2D.Double((int) -(branchThickness / 2), (int) ((int) -(subBranches[i].getLength())-(currentSubBranchParent.getLength()))),new Point2D.Double()));
+                    Point subBranchCoordinates = new Point((branchCoordinates.x), (int) (branchCoordinates.y - (branchLength) / 2));
+                    //AffineTransform old = g2d.getTransform();
+                    //g2d.translate(subBranchCoordinates.x, subBranchCoordinates.y);
+                    g2d.rotate(Math.toRadians(subBranches[i].getAngle()));
+                    g2d.fillRect((int) -(branchThickness / 2), (int) ((int) -(subBranches[i].getLength())-(currentSubBranchParent.getLength())), (int) branchThickness, (int) subBranches[i].getLength());
+                    g2d.setTransform(old);
+                }
             }
+            branchThickness-=branchThicknessDecreaseAmount;
         }
     }
 }
--- src/com/oreconsultants/SubBranch.java
+++ src/com/oreconsultants/SubBranch.java
@@ -4,7 +4,7 @@ public class SubBranch {
     private final double angle;
     private Branch branchParent;
     private SubBranch subParent;
-    private final boolean parentType; // false = Branch, True = SubBranch
+    private boolean parentType; // false = Branch, True = SubBranch
     private Branch child; // I may make the code, so you can have a child of a subBranch, but... How will we know their angle and have it stored easily
     private double length;
     SubBranch (double angle, double length, Branch parent, Branch child) {
@@ -48,10 +48,32 @@ public class SubBranch {
     public void setLength(double length) {
         this.length = length;
     }
+    /**
+     * This will override and set the <code>SubBranch</code> parent to be <code>null</code>.
+     * Because there can't be two parents for one branch... at least yet.
+     * @param branchParent The <code>Branch</code> to set as this SubBranches Parent.
+     **/
     public void setBranchParent(Branch branchParent) {
+        this.parentType = false; // setting the parentType to honour the new branch.
         this.branchParent = branchParent;
+        this.subParent = null; // setting it to null because we can't have two parents
     }
+    /**
+     * This will override and set the <code>Branch</code> parent to be <code>null</code>.
+     * Because there can't be two parents for one branch... at least yet.
+     * @param subParent The <code>Branch</code> to set as this SubBranches Parent.
+     **/
     public void setSubParent(SubBranch subParent) {
+        this.parentType = true;
         this.subParent = subParent;
+        this.branchParent = null;
+    }
+    /**
+     * The <code>ParentType</code> variable is either 'true' or 'false', which makes sense
+     * because it is a boolean. Anyway, that's all I have to say.
+     * @return True if the parent is a <code>SubBranch</code>, false if the parent is a <code>Branch</code>
+     **/
+    public boolean getParentType() {
+        return parentType;
     }
 }
